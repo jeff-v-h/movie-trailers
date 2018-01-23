318957dcc9eee1b0cabb13d4b084745bfb717a83
Jeff <jh0791@gmail.com>
2018-01-24 09:26:15 +1100

    Fix python style issues

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,36 +1,46 @@
 import fresh_tomatoes
 import media
 
+
+# Forrest Gump movie: movie title, poster image and movie trailer
 forrest_gump = media.Movie(
     "Forrest Gump",
     "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",
     "https://www.youtube.com/watch?v=bLvqoHBptjg")
 
+# Lion King movie: movie title, poster image and movie trailer
 the_lion_king = media.Movie(
     "The Lion King",
     "https://upload.wikimedia.org/wikipedia/en/3/3d/The_Lion_King_poster.jpg",
     "https://www.youtube.com/watch?v=4sj1MT05lAA")
 
+# Shawshank Redemption movie: movie title, poster image and movie trailer
 the_shawkshank_redemption = media.Movie(
     "The Shawshank Redemption",
-    "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",  # NOQA
     "https://www.youtube.com/watch?v=6hB3S9bIaco")
 
+# Ex Machina movie: movie title, poster image and movie trailer
 ex_machina = media.Movie(
     "Ex Machina",
     "https://upload.wikimedia.org/wikipedia/en/b/ba/Ex-machina-uk-poster.jpg",
     "https://www.youtube.com/watch?v=sNExF5WYMaA")
 
+# I Am Sam movie: movie title, poster image and movie trailer
 i_am_sam = media.Movie(
     "I Am Sam",
     "http://img.moviepostershop.com/i-am-sam-movie-poster-2001-1020204010.jpg",
     "https://www.youtube.com/watch?v=_A7N1a7TPbI")
 
+# The Dark Knight movie: movie title, poster image and movie trailer
 the_dark_knight = media.Movie(
     "The Dark Knight",
     "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
     "https://www.youtube.com/watch?v=TQfATDZY5Y4")
 
+# Set the movies that will be passed to the media file
 movies = [forrest_gump, the_lion_king, the_shawkshank_redemption, ex_machina,
           i_am_sam, the_dark_knight]
+
+# Open the HTML file in a webbrowser via the fresh_tomatoes.py file
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,18 +1,21 @@
 import webbrowser
 
+
 class Movie():
-	''' This class provides a way to store movie related information.
+    '''This class provides a way to store movie related information.
+    Attributes:
+        movie_title: The movie's title.
+        poster_image_url: a url link to a poster for the movie
+        trailer_youtube_id: a url link to a trailer for the movie on youtube
+    '''
 
-	Attributes:
-		movie_title: The movie's title.
-		poster_image_url: a url link to a poster for the movie
-		trailer_youtube_id: a url link to a trailer for the movie on youtube
-	'''
+    def __init__(self, movie_title, poster_url, youtube_trailer):
+        '''This constructor method initialises a movie instance with the
+        movie's title, poster url and url to the trailer from youtube '''
+        self.title = movie_title
+        self.poster_image_url = poster_url
+        self.trailer_youtube_url = youtube_trailer
 
-	def __init__(self, movie_title, poster_url, youtube_trailer):
-		self.title = movie_title
-		self.poster_image_url = poster_url
-		self.trailer_youtube_url = youtube_trailer
-
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+    def show_trailer(self):
+        ''' This function opens the movie's youtube trailer in a webbrowser '''
+        webbrowser.open(self.trailer_youtube_url)
